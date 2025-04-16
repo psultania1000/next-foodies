9d5f3bf902ac3a28ed0cba1b1772670c2ab2f4e8
Priyansh Sultania <psultania1000@gmail.com>
2025-04-16 11:23:13 +0530

    added optimized nav

--- app/components/main-header/main-header.jsx
+++ app/components/main-header/main-header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import LogoImg from "@/assets/logo.png";
-import Link from "next/link";
 import classes from "./main-header.module.css";
 import Image from "next/image";
 import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
+import Link from "next/link";
 const MainHeader = () => {
   return (
     <>
@@ -17,10 +18,10 @@ const MainHeader = () => {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <NavLink href="/meals">Browse Meals</NavLink>
             </li>
             <li>
-              <Link href="community">Foodies Community</Link>
+              <NavLink href="/community">Foodies Community</NavLink>
             </li>
           </ul>
         </nav>
